06b73f545800ff3b79ce108324fdc9d94bbe522e
Cortalo <Cortalo@outlook.com>
2025-08-20 21:26:38 +0200

    init

---
+++ testAllCachePolicy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <vector>
+#include <iomanip>
+#include <random>
+#include <algorithm>
+#include <array>
+
+
+int main() {
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
